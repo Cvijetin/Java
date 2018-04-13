1222b1ed31f75d8860c9f4f0e7ac181259c491de
Cvijetin Vidakovic <vidakovic.cvijetin@gmail.com>
2018-04-13 08:17:15 +0200

    treći

---
+++ treci.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Start;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Cvijetin
+ */
+public class Drugi {
+
+    public static void main(String[] args) {
+//DRUGI ZADATAK
+        for (int i = 1; i <= 12; i++) {
+            for (int j = 1; j <= 12; j++) {
+                System.out.println(i * j + " ");
+
+            }
+            System.out.println("");
+        }
+
+    }
+}
