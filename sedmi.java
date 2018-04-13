b81ded6f631233d483e6104e83d26196bd9c7165
Cvijetin Vidakovic <vidakovic.cvijetin@gmail.com>
2018-04-13 20:58:11 +0200

    Sedmi
    Sedmi zadatak

---
+++ sedmi.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Start;
+
+/**
+ *
+ * @author Cvijetin
+ */
+public class Sedmi {
+
+    public static void main(String args[]) {
+        char[] arr = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                System.out.print(arr[i]);
+            }
+        }
+    }
+}
